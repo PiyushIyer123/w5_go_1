b437a2f62504da996ab4b5d9698a7d57f3d62065
PiyushIyer123 <iyerpiyush07@gmail.com>
2024-09-26 13:23:37 -0400

    add and divide

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
 	Greeting()
 	advanceGreeting("M")
+	fmt.Println(add(1, 2))
+	c := add(1, 2)
+	d := 5
+	e := c + d
+	fmt.Println(e)
+	q, r := divide(10, 3)
+	// var q int
+	// q = 10
+	// var q int = 10
+	fmt.Printf("Quotient: %d, REminder: %d\n", q, r)
+	q, r = 20, 3
+	fmt.Printf("Quotient: %d, REminder: %d\n", q, r)
+
 }
